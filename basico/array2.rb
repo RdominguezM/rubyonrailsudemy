fc4f6fa07b1bb8995b1ea3aa6afa56baaa70a036
ramondm <rdominguezm02@gmail.com>
2020-02-07 16:30:05 -0400

    commit error

---
+++ basico/array2.rb
@@ -0,0 +1,12 @@
+sabor = "fresa"
+puts [123,123]
+vec4 = [80.5, sabor, [true,false]]
+puts vec4[2]
+
+nombres1 = ['ann', 'richad', 'william', 'susan', 'pat' ]
+puts nombres1[0]
+puts nombres1[3]
+
+nombres2 = %w{ ann richard william susan pat }
+puts nombres2
+
---
+++ basico/unless.rb
@@ -0,0 +1,10 @@
+if "jesus" != "alberto"
+puts "son diferentes"
+end
+
+nombre = gets.chomp
+unless nombre == "alberto"
+puts "No coincide"
+else 
+    puts "coincide"
+end
